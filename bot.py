a4908be531664b68099eae4e5e18c094e8d81cfc
ris-tlp <omarkhantlp@gmail.com>
2018-10-17 00:12:53 +0300

    added join/exit event messages

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 prefix = (".", "!")
-TOKEN = ''
+TOKEN = '' 
 
 bot = commands.Bot(command_prefix = prefix)
 
@@ -12,6 +12,16 @@ async def on_ready():
 
 @bot.command(pass_context = True)
 async def ping(ctx):
-    await bot.say("pong!") #test
+    await bot.say("pong!") # for testing #
+
+@bot.event
+async def on_member_join(member):
+    channel = member.server.get_channel("") #put channel id
+    await bot.send_message(channel, "Welcome to the server, {0}".format(member.mention))
+
+@bot.event
+async def on_member_remove(member):
+    channel = member.server.get_channel("") #put channel id
+    await bot.send_message(channel, "{0} has left the server.".format(member.mention))
 
 bot.run(TOKEN)
